ceccb76817db9bcc8e7ab19242eb9803c0412f34
Himanshu <153494576+himanshu0470@users.noreply.github.com>
2024-03-16 17:17:29 +0530

    Checkin0923134

---
+++ src/components/Common/Reusables/ListingComponent.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, } from "react";
+import "../Table/style.css"
+import { useNavigate } from "react-router-dom";
+import "jspdf-autotable";
+import { Button, Card, CardBody, CardHeader, Col, Row } from "reactstrap";
+import { useDispatch } from "react-redux";
+
+export const ListingElement = ({ columns, dataSource, tableElement, }) => {
+    document.title = `${tableElement?.title}`;
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    return (
+        <Row>
+            <Col lg={12}>
+                <Card>
+                    <CardBody>
+                        <div id="customerList">
+                            <Row className="g-2 d-flex align-items-center">
+                                <Col className="col-sm-auto">
+                                    <span>
+                                        Showing {dataSource?.length} entries
+                                    </span>
+                                    <div className="d-flex align-items-center justify-content-end"></div>
+                                </Col>
+                            </Row>
+
+                            <div
+                                className="table-responsive table-card mt-3 mb-1"
+                                id="myTable"
+                            >
+
+                                <table
+                                    className="table align-middle table-nowrap"
+                                    id="customerTable"
+                                >
+                                    <thead className="table-light">
+                                        <tr>
+                                            {columns.map((column) => (
+                                                <th key={column.key} style={column.style}>
+                                                    <div className="d-flex">
+                                                        <span>{column.title}</span>
+                                                    </div>
+                                                </th>
+                                            ))}
+                                        </tr>
+                                    </thead>
+                                    <tbody className="list form-check-all">
+                                        {dataSource.map((record, index) => (
+                                            <tr key={index} className={`hover`}>
+                                                {columns.map((column) => (
+                                                    <td key={column.key} style={column.style}>
+                                                        {column.render
+                                                            ? column.render(
+                                                                record[column.dataIndex],
+                                                                record
+                                                            )
+                                                            : record[column.dataIndex]}
+                                                    </td>
+                                                ))}
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+
+                                <div className="noresult" style={{ display: "none" }}>
+                                    <div className="text-center">
+                                        <lord-icon
+                                            src="https://cdn.lordicon.com/msoeawqm.json"
+                                            trigger="loop"
+                                            colors="primary:#121331,secondary:#08a88a"
+                                            style={{ width: "75px", height: "75px" }}
+                                        ></lord-icon>
+                                        <h5 className="mt-2">Sorry! No Result Found</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+    );
+}
