5eb806631c9d1a1d1404e144e1bb8c5e9941762a
Himanshu <153494576+himanshu0470@users.noreply.github.com>
2024-03-16 23:43:15 +0530

    sdfgh

--- src/Pages/Commentary/MarketEventAction.jsx
+++ src/Pages/Commentary/MarketEventAction.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ERROR, PERMISSION_VIEW, SUCCESS, TAB_COMMENTARY } from "../../components/Common/Const";
 import { checkPermission } from "../../components/Common/Reusables/reusableMethods";
 import Breadcrumbs from "../../components/Common/Breadcrumb";
-import { Button, Card, CardBody, CardHeader, Col, Container, Input, Row } from "reactstrap";
+import { Button, Card, CardBody, Col, Container, Input, Row } from "reactstrap";
 import SpinnerModel from "../../components/Model/SpinnerModel";
 import { updateToastData } from "../../Features/toasterSlice";
 import axiosInstance from "../../Features/axios";
 import { MARKET_STATUS } from "./CommentartConst";
 import { ListingElement } from "../../components/Common/Reusables/ListingComponent";
+import "./CommentaryCss.css"
 
 export const MarketEventAction = () => {
     const pageName = TAB_COMMENTARY;
@@ -19,14 +20,7 @@ export const MarketEventAction = () => {
     const [isLoading, setIsLoading] = useState(false);
     const commentaryId = location.state?.commentaryId || "0";
     const dispatch = useDispatch();
-    const [checekedList, setCheckedList] = useState([]);
     const [data, setData] = useState([]);
-    const [allInnings, setAllInnings] = useState([]);
-    const [allTeams, setAllTeams] = useState([]);
-
-    useEffect(() => {
-        setCheckedList(data.filter(i => i.isCreate).map(i => i.index))
-    }, [data])
 
     const handleValueChange = (record, key, value) => {
         const indexOfData = data.findIndex(i => i.index === record.index)
@@ -66,55 +60,29 @@ export const MarketEventAction = () => {
     const fetchData = async (commentaryId) => {
         setIsLoading(true);
         await axiosInstance
-            .post("/admin/eventMarket/getDetailsByCId", { commentaryId })
+            .post("/admin/eventMarket/marketListByCId", { commentaryId })
             .then((response) => {
                 if (response?.result) {
-                    const teamAndPlayers = response?.result?.teamAndPlayers;
-                    const marketTemplate = response?.result?.marketTemplate;
-                    const commentary = response?.result?.commentary;
-                    const predefinedMarket = marketTemplate?.filter(value => value?.isPredefineMarket);
-                    const eventMarket = response?.result?.eventMarket;
-                    if (teamAndPlayers?.length) {
-                        const uniqueInnings = teamAndPlayers?.filter(value => value.teamId === teamAndPlayers[0].teamId)
-                        setAllInnings(uniqueInnings.map(option => ({ label: `Inning ${option.currentInnings}`, value: option.currentInnings })))
-                        const uniqueTeams = teamAndPlayers?.filter(value => value.currentInnings === 1)
-                        setAllTeams(uniqueTeams.map(option => ({ label: option.shortName, value: option.teamId })))
-                    }
-                    let newData = [];
-                    predefinedMarket.forEach((market, marketIndex) => {
-                        teamAndPlayers.forEach((team, teamIndex) => {
-                            newData.push(({
-                                eventMarketId: "0",
-                                index: marketIndex * teamAndPlayers.length + teamIndex,
-                                isCreate: true,
-                                status: "1",
-                                overRate: "",
-                                underRate: "",
-                                margin: "",
-                                line: "",
-                                isAllow: true,
-                                data: "", // not getting from market
-                                playerId: null, // not getting from market
-                                ...market,
-                                commentaryId: commentary.commentaryId,
-                                eventRefId: commentary.eventRefId,
-                                market: market.templateName,
-                                teamId: team.teamId,
-                                inningsId: team.currentInnings,
-                            }))
-                        });
-                    });
-                    newData = newData.map((market) => {
-                        const eventMarketIndex = eventMarket.findIndex((value) => market.over == value.over && market.teamId == value.teamId && market.inningsId == value.inningsId)
-                        if (eventMarketIndex !== -1) {
-                            return {
-                                ...market,
-                                ...eventMarket[eventMarketIndex]
-                            }
+                    console.log(response?.result)
+                    const dataList = response?.result || []
+                    let updatedDatalist = dataList.map(eventMarket => {
+                        return {
+                            ...eventMarket,
+                            ...eventMarket.marketRunners[0]
                         }
-                        return market;
                     })
-                    setData(newData);
+                    // const updatedDataList = dataList.
+                    // newData = newData.map((market) => {
+                    //     const eventMarketIndex = eventMarket.findIndex((value) => market.over == value.over && market.teamId == value.teamId && market.inningsId == value.inningsId)
+                    //     if (eventMarketIndex !== -1) {
+                    //         return {
+                    //             ...market,
+                    //             ...eventMarket[eventMarketIndex]
+                    //         }
+                    //     }
+                    //     return market;
+                    // })
+                    setData(updatedDatalist);
                 }
                 setIsLoading(false);
             })
@@ -127,55 +95,44 @@ export const MarketEventAction = () => {
     const handleBackClick = () => {
         navigate("/commentary");
     };
-    const fetchTeamName = (id) => {
-        const teamOption = allTeams.find(element => element.value === id)
-        return teamOption.value
-    }
+    // const fetchTeamName = (id) => {
+    //     const teamOption = allTeams.find(element => element.value === id)
+    //     return teamOption.label
+    // }
     //elements required
     const tableElement = {
         title: "Predefined",
         displayTitle: true
     };
-
-    //table columns
+    const saveData = (records = []) => {
+        records.forEach(data => {
+            console.log(data)
+        })
+    }
     const columns = [
         {
             title: "Inning",
             dataIndex: "inningsId",
             render: (text) => (<span>{`Innings ${text}`}</span>),
             key: "inningsId",
-            sort: true,
-            style: { width: "10%" },
         },
         {
             title: "Team",
             dataIndex: "teamId",
-            render: (text) => (<span>{fetchTeamName(text)}</span>),
+            // render: (text) => (<span>{fetchTeamName(text)}</span>),
             key: "teamId",
-            sort: true,
-            style: { width: "10%" },
         },
         {
             title: "Market",
-            dataIndex: "market",
-            render: (text, record) => (
-                <Input
-                    className="form-control"
-                    type="text"
-                    value={text || ""}
-                    onChange={(e) => handleValueChange(record, "market", e.target.value)}
-                />
-            ),
-            key: "market",
-            sort: true,
-            style: { width: "10%" },
+            dataIndex: "marketName",
+            key: "marketName",
         },
         {
             title: "Status",
             dataIndex: "status",
             render: (text, record) => (
                 <select
-                    className="form-select"
+                    className="form-control small-text-fields"
                     value={text}
                     onChange={(e) => {
                         handleValueChange(record, "status", e.target.value);
@@ -188,71 +145,114 @@ export const MarketEventAction = () => {
                 </select>
             ),
             key: "status",
-            sort: true,
-            style: { width: "10%" },
-        },
-        {
-            title: "Over",
-            dataIndex: "overRate",
-            render: (text, record) => (
-                <Input
-                    className="form-control"
-                    type="text"
-                    value={text || ""}
-                    onChange={(e) => handleValueChange(record, "overRate", e.target.value)}
-                />
-            ),
-            key: "overRate",
-            sort: true,
-            style: { width: "10%" },
         },
         {
             title: "Line",
             dataIndex: "line",
             render: (text, record) => (
                 <Input
-                    className="form-control"
-                    type="text"
+                    className="form-control small-text-fields"
+                    type="number"
+                    step={1}
+                    min={0}
                     value={text || ""}
                     onChange={(e) => handleValueChange(record, "line", e.target.value)}
                 />
             ),
             key: "line",
-            sort: true,
-            style: { width: "10%" },
+        },
+        {
+            title: "Over",
+            dataIndex: "overRate",
+            render: (text, record) => (
+                <Input
+                    className="form-control small-text-fields"
+                    type="number"
+                    step={1}
+                    min={0}
+                    value={text || ""}
+                    onChange={(e) => handleValueChange(record, "overRate", e.target.value)}
+                />
+            ),
+            key: "overRate",
         },
         {
             title: "Under",
             dataIndex: "underRate",
             render: (text, record) => (
                 <Input
-                    className="form-control"
-                    type="text"
+                    className="form-control small-text-fields"
+                    type="number"
+                    step={1}
+                    min={0}
                     value={text || ""}
                     onChange={(e) => handleValueChange(record, "underRate", e.target.value)}
                 />
             ),
             key: "underRate",
-            sort: true,
-            style: { width: "10%" },
         },
         {
-            title: "Margin",
-            dataIndex: "margin",
+            title: "No Rate",
+            dataIndex: "noRate",
             render: (text, record) => (
                 <Input
-                    className="form-control"
-                    type="text"
+                    className="form-control small-text-fields"
+                    type="number"
+                    step={1}
+                    min={0}
                     value={text || ""}
-                    onChange={(e) => handleValueChange(record, "margin", e.target.value)}
+                    onChange={(e) => handleValueChange(record, "noRate", e.target.value)}
                 />
             ),
-            key: "margin",
-            sort: true,
-            style: { width: "10%" },
+            key: "noRate",
         },
         {
-            title: "Is Active",
+            title: "No Point",
+            dataIndex: "noPoint",
+            render: (text, record) => (
+                <Input
+                    className="form-control small-text-fields"
+                    type="number"
+                    step={1}
+                    min={0}
+                    value={text || ""}
+                    onChange={(e) => handleValueChange(record, "noPoint", e.target.value)}
+                />
+            ),
+            key: "noPoint",
+        },
+        {
+            title: "Yes Rate",
+            dataIndex: "yesRate",
+            render: (text, record) => (
+                <Input
+                    className="form-control small-text-fields"
+                    type="number"
+                    step={1}
+                    min={0}
+                    value={text || ""}
+                    onChange={(e) => handleValueChange(record, "yesRate", e.target.value)}
+                />
+            ),
+            key: "yesRate",
+        },
+        {
+            title: "Yes Point",
+            dataIndex: "yesPoint",
+            render: (text, record) => (
+                <Input
+                    className="form-control small-text-fields"
+                    type="number"
+                    step={1}
+                    min={0}
+                    value={text || ""}
+                    onChange={(e) => handleValueChange(record, "YesPoint", e.target.value)}
+                />
+            ),
+            key: "YesPoint",
+        },
+        {
+            title: "Active",
             dataIndex: "isActive",
             render: (text, record) => (
                 <Button
@@ -270,7 +270,7 @@ export const MarketEventAction = () => {
             style: { width: "2%", textAlign: "center" },
         },
         {
-            title: "Market Allow",
+            title: "Allow",
             dataIndex: "isAllow",
             render: (text, record) => (
                 <Button
@@ -286,6 +286,33 @@ export const MarketEventAction = () => {
             ),
             key: "isAllow",
             style: { width: "2%", textAlign: "center" },
+        },
+        {
+            title: "Is Send",
+            dataIndex: "isSend",
+            render: (text, record) => (
+                <Button
+                    color={`${record.isActive ? "primary" : "danger"}`}
+                    size="sm"
+                    className="btn"
+                    onClick={() => {
+                        handleValueChange(record, "isSend", !record.isActive);
+                    }}
+                >
+                    <i className={`bx ${record.isSend ? "bx-check" : "bx-block"}`}></i>
+                </Button>
+            ),
+            key: "isSend",
+            style: { width: "2%", textAlign: "center" },
+        },
+        {
+            title: "Save",
+            render: (text, record) => (
+                <Button color="primary" className="btn" onClick={() => saveData(record)}>
+                    <i>Save</i>
+                </Button>
+            ),
+            key: "isSend",
         },
     ];
 
@@ -308,7 +335,7 @@ export const MarketEventAction = () => {
                                 {isLoading && <SpinnerModel />}
                                 <Row className='mb-3'>
                                     <Col className="mt-3 mt-lg-4 mt-md-4">
-                                        <Breadcrumbs title="ScoreCard" breadcrumbItem="Commentary Market Template" page="updatecp" />
+                                        <Breadcrumbs title="ScoreCard" breadcrumbItem="Open Market" page="updatecp" />
                                     </Col>
                                     <Col className="mt-3 mt-lg-3 mt-md-3">
                                         <button className="btn btn-danger text-right" onClick={handleBackClick}>Back</button>
