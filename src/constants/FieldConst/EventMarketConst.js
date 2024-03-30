640743809a4eae13bb5810ab908424a7b59fe97c
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2024-03-30 21:03:07 +0530

    added delay in event market & market template page

--- src/constants/FieldConst/EventMarketConst.js
+++ src/constants/FieldConst/EventMarketConst.js
@@ -330,6 +330,14 @@ export const EventMarketFields = [
     fieldColspan: { xs: 12, md: 4, lg: 4 },
   },
   {
+    type: TEXT,
+    name: "delay",
+    label: "Market Delay",
+    isRequired: true,
+    labelColspan: { xs: 12, md: 2, lg: 2 },
+    fieldColspan: { xs: 12, md: 4, lg: 4 },
+  },
+  {
     type: SWITCH,
     name: "isActive",
     label: "Is Active",
