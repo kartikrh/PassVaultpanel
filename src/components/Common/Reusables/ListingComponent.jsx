e646dbd3a12ce0eafab2422e29837f796c0dcf8b
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2024-05-11 18:03:10 +0530

    added status wise colour in open market page

--- src/components/Common/Reusables/ListingComponent.jsx
+++ src/components/Common/Reusables/ListingComponent.jsx
@@ -4,6 +4,25 @@ import { Card, CardBody, Col, Row } from "reactstrap";
 
 export const ListingElement = ({ columns, dataSource = [], tableElement, tableExtras }) => {
     document.title = `${tableElement?.title}`;
+    const getStatusColor = (status) => {
+        switch (status) {
+            case 1: 
+                return "#d7eed7"; //light green (open)
+            case 2: 
+                return "#ece9e9"; //light gray (inActive)
+            case 3: 
+                return "#f6ddcf"; //light orange (suspend)
+            case 4: 
+                return "#d7ebf1"; //light blue (close)
+            case 5: 
+                return "#f9f9e8"; //light yellow (settled)
+            case 6:
+                return "#f6e0e0"; //light red (cancel)
+            default:
+                return ""; 
+        }
+    };
+    
     return (
         <Row>
             <Col lg={12}>
@@ -41,7 +60,7 @@ export const ListingElement = ({ columns, dataSource = [], tableElement, tableEx
                                     </thead>
                                     <tbody className="list form-check-all">
                                         {dataSource.map((record, index) => (
-                                            <tr key={index}>
+                                            <tr key={index} style={{ backgroundColor: getStatusColor(+record?.status) }}>
                                                 {columns.map((column, index) => (
                                                     <td key={index}
                                                         style={column.style}
