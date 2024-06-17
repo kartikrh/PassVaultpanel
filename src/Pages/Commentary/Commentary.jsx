8c47a88177aa321511371af0af81c876333a543c
Himanshu <153494576+himanshu0470@users.noreply.github.com>
2024-06-17 10:38:40 +0530

    wicket issue

--- src/Pages/Commentary/Commentary.jsx
+++ src/Pages/Commentary/Commentary.jsx
@@ -290,7 +290,6 @@ export const CommentaryScreen = ({
                     setIsBoundary(undefined)
                 }}
             />}
-
         {actionPopup && <CommentaryAction
             toggle={() => setActionPopup(false)}
             changeOver={() => {
