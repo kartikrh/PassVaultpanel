f11650b986540447666ebab90852fb8ace4c4dcd
Himanshu <153494576+himanshu0470@users.noreply.github.com>
2024-08-08 21:10:35 +0530

    bug fix

--- src/Pages/Commentary/CommentaryModels/SelectPlayerModal.jsx
+++ src/Pages/Commentary/CommentaryModels/SelectPlayerModal.jsx
@@ -11,7 +11,7 @@ const SelectPlayerModal = ({ playerList, toggle, isOpen, selectPlayer }) => {
     }, [isOpen, playerList]);
 
     useEffect(() => {
-        const filteredPlayers = playerList?.filter(value => value.playerName.toLowerCase().includes(search.toLowerCase()));
+        const filteredPlayers = playerList?.filter(value => value.playerName?.toLowerCase().includes(search.toLowerCase()));
         setPlayers(filteredPlayers)
     }, [search])
 
