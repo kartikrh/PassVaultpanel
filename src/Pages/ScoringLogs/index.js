3a6bb7bc7b16ab172686d116a5f2c11bc86aac0e
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2024-09-05 15:49:47 +0530

    modified scoring logs

---
+++ src/Pages/ScoringLogs/index.js
@@ -0,0 +1,269 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
+import { useSelector } from "react-redux";
+import { PERMISSION_VIEW, TAB_SCORING_LOGS } from "../../components/Common/Const";
+import Table from "../../components/Common/Table";
+import SpinnerModel from "../../components/Model/SpinnerModel";
+import axiosInstance from "../../Features/axios";
+import { checkPermission, convertDateUTCToLocal } from "../../components/Common/Reusables/reusableMethods";
+import Breadcrumbs from "../../components/Common/Breadcrumb";
+import { mapCommentaryStatus } from "../Commentary/functions";
+
+function ScoringLogs() {
+  const pageName = TAB_SCORING_LOGS;
+  const finalizeRef = useRef(null);
+  document.title = "Score Access Logs";
+  const [data, setData] = useState([]);
+  const [checekedList, setCheckedList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const permissionObj = useSelector((state) => state.auth?.tabPermissionList);
+  const [eventTypes, setEventTypes] = useState([]);
+  const [competitions, setCompetitions] = useState([]);
+  const [commentary, setCommentary] = useState([]);
+  const [isSearch, setIsSearch] = useState(true);
+  const [dateRange, setDateRange] = useState({
+    startDate: `${new Date().toISOString().split("T")[0]}T00:00:00`,
+    endDate: `${new Date().toISOString().split("T")[0]}T23:59:00`,
+  });
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const [total, setTotal] = useState(0);
+  let navigate = useNavigate();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const commentaryId = queryParams.get('commentaryId') || 0;
+
+  const fetchData = async (latestValueFromTable) => {
+    setIsLoading(true);
+    const tableActions = finalizeRef.current.getTableAction();
+    let payload = {
+      ...(latestValueFromTable || tableActions),
+      page: currentPage+1,
+      limit: pageSize,
+    }
+    if(commentaryId !== 0) {
+      payload = {
+        ...(latestValueFromTable || tableActions),
+        page: currentPage+1,
+        limit: pageSize,
+        commentaryId: commentaryId
+      };
+    }
+    if (isSearch) {
+      payload = {
+        ...payload,
+        ...dateRange,
+      };
+    }
+    await axiosInstance
+      .post("/admin/commentaryScoringLogs/all", payload)
+      .then(async (response) => {
+        const apiData = response?.result?.data;
+        let apiDataIdList = [];
+        apiData.forEach((ele) => {
+          apiDataIdList.push(ele?.id);
+        });
+        setData(apiData);
+        setTotal(response?.result?.totalPages || 0);
+        setCheckedList([]);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+      });
+      if (latestValueFromTable?.eventTypeId) {
+        fetchCompetitionData(latestValueFromTable?.eventTypeId);
+      }
+      if(latestValueFromTable?.competitionId) {
+        fetchCommentaryData(latestValueFromTable?.competitionId);
+      }
+  };
+
+  useEffect(()=>{
+    if(commentaryId !== 0){
+      setIsSearch(false)
+    } else {
+      setIsSearch(true)
+    }
+  },[commentaryId])
+
+  const fetchEventTypeData = async () => {
+    await axiosInstance
+      .post(`/admin/log/eventTypeList`, { isActive: true })
+      .then((response) => {
+        setEventTypes(response.result);
+      })
+      .catch((error) => { });
+  };
+  const fetchCompetitionData = async (value) => {
+    await axiosInstance
+      .post(`/admin/log/competitionListByEventTypeId`, {
+        eventTypeId: value,
+      })
+      .then((response) => {
+        setCompetitions(response.result);
+      })
+      .catch((error) => { });
+  };
+  const fetchCommentaryData = async (value) => {
+    await axiosInstance
+      .post(`/admin/log/getComByCompetition`, {
+        competitionId: value,
+      })
+      .then((response) => {
+        setCommentary(response.result);
+      })
+      .catch((error) => { });
+  };
+
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "eventDate",
+      render: (text, record) => (
+        <span>
+          {convertDateUTCToLocal(text, "index")}
+        </span>
+      ),
+      key: "eventDate",
+      sort: true,
+      style: { width: "10%" },
+    },
+    {
+      title: "Event Id",
+      dataIndex: "eventRefId",
+      key: "eventRefId",
+      sort: true,
+      style: { width: "10%" },
+    },
+    {
+      title: "Event",
+      dataIndex: "eventName",
+      key: "eventName",
+      sort: true,
+      style: { width: "10%" },
+    },
+    {
+      title: "Competition",
+      dataIndex: "competition",
+      key: "competition",
+      sort: true,
+      style: { width: "10%" },
+    },
+    {
+      title: "Status",
+      dataIndex: "commentaryStatus",
+      render: (text, record) => (
+        <span>{mapCommentaryStatus(text)}</span>
+      ),
+      key: "commentaryStatus",
+      sort: true,
+      style: { width: "10%" },
+    },
+    {
+      title: "User",
+      dataIndex: "userName",
+      key: "userName",
+      sort: true,
+      style: { width: "10%" },
+    },
+    {
+      title: "Time",
+      dataIndex: "createdDate",
+      render: (text, record) => (
+        <span>
+          {convertDateUTCToLocal(text, "index")}
+        </span>
+      ),
+      key: "createdDate",
+      sort: true,
+      style: { width: "10%" },
+    },
+  ];
+
+  const handleBackClick = () => {
+    navigate("/commentary");
+  };
+
+  //elements required
+  const tableElement = {
+    title: "Score Access Logs",
+    eventTypeSelect: true,
+    competitionsSelect: true,
+    commentarySelect: true,
+    resetButton: true,
+    reloadButton: true,
+    isServerPagination: true,
+    isDateRange: true,
+  };
+
+  useEffect(() => {
+    if (!checkPermission(permissionObj, pageName, PERMISSION_VIEW)) {
+      navigate("/dashboard");
+    }
+    fetchData();
+    fetchEventTypeData();
+  }, [isSearch, currentPage, pageSize]);
+
+  const handleReset = (value) => {
+    fetchData();
+    fetchEventTypeData();
+  };
+
+  const handleReload = (value) => {
+    fetchData();
+    fetchEventTypeData();
+  };
+
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid={true}>
+          {isLoading && <SpinnerModel />}
+
+          <Row>
+            <Col className="mt-3 mt-lg-4 mt-md-4">
+              <Breadcrumbs
+                title="Commentary"
+                breadcrumbItem="Score Access Logs"
+              />
+            </Col>
+            <Col className="mt-3 mt-lg-3 mt-md-3">
+              <button
+                className="btn btn-danger text-right"
+                onClick={handleBackClick}
+              >
+                Back
+              </button>
+            </Col>
+          </Row>
+          <Table
+            ref={finalizeRef}
+            columns={columns}
+            dataSource={data}
+            tableElement={tableElement}
+            singleCheck={checekedList}
+            reFetchData={fetchData}
+            eventTypes={eventTypes}
+            competitions={competitions}
+            commentary={commentary}
+            handleReset={handleReset}
+            handleReload={handleReload}
+            setDateRange={setDateRange}
+            dateRange={dateRange}
+            serverCurrentPage={currentPage}
+            serverPageSize={pageSize}
+            serverTotal={total}
+            setServerCurrentPage={setCurrentPage}
+            setServerPageSize={setPageSize}
+            isSearch={isSearch}
+            setIsSearch={setIsSearch}
+          />
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default ScoringLogs;
