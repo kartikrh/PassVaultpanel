d680f04b56f016af41a0b895168c623d1ba34067
Himanshu <153494576+himanshu0470@users.noreply.github.com>
2024-10-16 13:41:34 +0530

    updated runner thing

--- src/Pages/Commentary/MultiRunnerMarket.jsx
+++ src/Pages/Commentary/MultiRunnerMarket.jsx
@@ -195,7 +195,7 @@ const MultiRunnerMarket = ({ market, onUpdate, teams, handleSingleAction, loadin
                 <tbody className='whitespace-nowrap '>
                     {sortedRunners.map((runner, index) => (
                         <tr key={runner.runnerId}>
-                            <td>{teams[runner.teamId] || `Runner ${runner.runnerId}`}</td>
+                            <td>{teams[runner.teamId] || `${runner.runnerId} - ${runner.runner}`}</td>
                             <td>
                                 <select
                                     className="form-control small-text-fields"
