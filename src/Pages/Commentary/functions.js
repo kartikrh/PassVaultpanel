841d516899ee1c5f5b2c2bc608880f039b749506
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2024-10-23 10:54:18 +0530

    modified generate over under function

--- src/Pages/Commentary/functions.js
+++ src/Pages/Commentary/functions.js
@@ -235,8 +235,8 @@ export const fetchNextPlayerOrder = (playerType, playerList) => {
 }
 
 export const generateOverUnder = (dataObj) => {
-  const roundedLine = Math.floor(parseFloat(dataObj?.line));
-  const thresholdValue = Math.floor(roundedLine) + 0.5;
+  const roundedLine = Math.round(parseFloat(dataObj?.line));
+  const thresholdValue = Math.floor(parseFloat(dataObj?.line)) + 0.5;
   const marginAdjustment = dataObj?.margin ? ((dataObj.margin / 100) + 1) : 1;
   const dataToSend = {
     ...dataObj,
