b1957e10fdd41610091832d133addba02d3b2b0f
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2024-10-24 19:50:27 +0530

    modified marketType condition

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import authReducer from "./Features/Authentication/authorizationSlice";
 import userReducer from "./Features/Authentication/userSlice";
 import tabReducer from "./Features/Tabs/combinedReducer"
 import toastReducer from "./Features/toasterSlice"
+import marketTypeReducer from "./Features/Authentication/marketTypeSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
     toastData: toastReducer,
     tabsData: tabReducer,
+    marketType: marketTypeReducer,
   }
 });
 
