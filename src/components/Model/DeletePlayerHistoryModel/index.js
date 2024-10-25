9ecbe74b942e763f835a4909e8b6f4817aa70de1
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2024-10-25 12:25:56 +0530

    added delete confirmation popup in player history

---
+++ src/components/Model/DeletePlayerHistoryModel/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Modal, ModalBody, ModalHeader } from "reactstrap";
+
+const Index = ({
+  deleteModelVisable,
+  setDeleteModelVisable,
+  handleDelete,
+  data,
+}) => {
+  const [modal_delete, setModal_delete] = useState(true);
+  function tog_close() {
+    setModal_delete(!modal_delete);
+  }
+  return (
+    <Modal
+      isOpen={deleteModelVisable}
+      toggle={() => {
+        setDeleteModelVisable(false);
+      }}
+      centered
+    >
+      <ModalHeader
+        className="bg-light p-3"
+        id="exampleModalLabel"
+        toggle={() => {
+          setDeleteModelVisable(false);
+        }}
+      >
+        Delete
+      </ModalHeader>
+      <div className="tablelist-form">
+        <ModalBody>
+          <div
+            className="d-flex flex-column justify-content-center align-items-center"
+            id="modal-id"
+          >
+            <span className="mt-4 mb-4">
+              Are you sure you want to delete the player {data} history?
+            </span>
+            <div className="hstack gap-2 justify-content-center">
+              <button
+                type="button"
+                className="btn btn-light"
+                onClick={() => {
+                  setDeleteModelVisable(false);
+                }}
+              >
+                Close
+              </button>
+              <button
+                className="btn btn-danger"
+                id="add-btn"
+                onClick={() => {
+                  handleDelete();
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </ModalBody>
+      </div>
+    </Modal>
+  );
+};
+
+export default Index;
