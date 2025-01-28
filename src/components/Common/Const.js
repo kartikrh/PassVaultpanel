45a99d8099bdf285a9e16cc7a6d374ec5cb00937
Himanshu <153494576+himanshu0470@users.noreply.github.com>
2025-01-28 21:37:06 +0530

    Update Const.js

--- src/components/Common/Const.js
+++ src/components/Common/Const.js
@@ -107,7 +107,7 @@ export const CONTENT_IMAGE_TYPE = {
   BLOCKS: "Blocks"
 }
 export const OPEN_MARKET_CONNECT = "connectEventMarket";
-export const COMMENTARY_STATUS_CONNECT = "connectEventMarket";
+export const COMMENTARY_STATUS_CONNECT = "conCommentary";
 export const OPEN_MARKET_DATA = "updateMarketData";
 export const UNDO_CALLED = "undoCalled";
 export const MARKET_RUNNER_CONNECT = "marketRunnerConnection";
