ecf08bae4a47f7c2884831bef7c09243ed4f7795
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2025-03-04 21:07:00 +0530

    modified dataprovider

--- src/Pages/DataProvider/Dataprovider.jsx
+++ src/Pages/DataProvider/Dataprovider.jsx
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import {
-    Typography,
-    Box,
-    CircularProgress,
-
-} from '@mui/material';
 import{Accordion, AccordionBody, AccordionItem, AccordionHeader, Table, Container, Row, Card, CardBody} from 'reactstrap'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import SportsCricketIcon from '@mui/icons-material/SportsCricket';
 import { useSelector } from 'react-redux';
 import { convertDateUTCToLocal } from '../../components/Common/Reusables/reusableMethods';
+import SpinnerModel from "../../components/Model/SpinnerModel";
 import { useNavigate } from 'react-router-dom';
 
 const DataproviderPage = () => {
     const [events, setEvents] = useState([]);
     const [groupedEvents, setGroupedEvents] = useState({});
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
     const [apiXkey, setApiXkey] = useState(null);
     const [apiURL, setApiURL] = useState(null);
     const loadInitData = useSelector((state) => state.loadInit.loadInitData);
@@ -64,10 +58,10 @@ const DataproviderPage = () => {
                     );
                     setEvents(sortedEvents);
                 } else {
-                    setError('Failed to fetch data');
+                    console.log('Failed to fetch data');
                 }
             } catch (err) {
-                setError('Error fetching data: ' + err.message);
+                console.log('Error fetching data: ' + err.message);
             } finally {
                 setLoading(false);
             }
@@ -97,22 +91,6 @@ const DataproviderPage = () => {
         navigate("/dataproviderMarkets");
     };
 
-    if (loading) {
-        return (
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                <CircularProgress />
-            </Box>
-        );
-    }
-
-    if (error) {
-        return (
-            <Box sx={{ p: 3, textAlign: 'center' }}>
-                <Typography color="error" variant="h5">{error}</Typography>
-            </Box>
-        );
-    }
-
     const getStatusLabel = (status) => {
         switch (status) {
             case 1: return "Yet to Start";
@@ -139,73 +117,28 @@ const DataproviderPage = () => {
                 <Row>
                     <Card className='p-0'>
                         <CardBody className='p-3'>
+                            {loading && <SpinnerModel />}
                             {Object.entries(groupedEvents).map(([eventType, competitions]) => (
                                 <Accordion key={eventType} className='data-provider-outer-accordian' open={openEventTypes} toggle={toggleEventType}>
-                                    {/* targetId={categoryId} */}
-                                    <AccordionItem >
+                                    <AccordionItem className="rounded-0">
                                     <AccordionHeader className='data-provider-outer-accordian-summary px-1' targetId={eventType}>
                                         <div className='d-flex align-items-center p-1 px-2'>
                                             <SportsCricketIcon sx={{ mr: 2 }} />
                                             <span style={{fontSize: '20px', fontWeight: 'bold'}}>{eventType}</span>
                                         </div>
                                     </AccordionHeader>
-                                    <AccordionBody accordionId={eventType} style={{padding: '0px'}} className='data-provider-outer-accordian-body'>
+                                    <AccordionBody accordionId={eventType} className='data-provider-outer-accordian-body p-0'>
                                         {Object.entries(competitions).map(([competition, matches]) => (
-                                            <Accordion key={competition} open={openCompetition} toggle={toggleCompetition} className='px-2 py-0'>
-                                                <AccordionItem>
+                                            <Accordion key={competition} open={openCompetition} toggle={toggleCompetition} className='category-list py-0'>
+                                                <AccordionItem className="rounded-0">
                                                 <AccordionHeader targetId={competition} className='data-provider-outer-accordian-summary' expandIcon={<ExpandMoreIcon />}>
                                                     <span style={{fontSize: '14px'}} className='p-1'>{competition}</span>
                                                 </AccordionHeader>
                                                 <AccordionBody accordionId={competition} className='ps-3'>
-                                                    {/* <TableContainer component={Paper}>
-                                                    <Table>
-                                                        <TableHead>
-                                                        <TableRow>
-                                                            <TableCell sx={{ width: "300px"}}><b>Date</b></TableCell>
-                                                            <TableCell sx={{ width: "500px"}}><b>Event</b></TableCell>
-                                                            <TableCell sx={{ width: "200px"}}><b>Status</b></TableCell>
-                                                        </TableRow>
-                                                        </TableHead>
-                                                        <TableBody>
-                                                        {matches.map((match) => (
-                                                        <TableRow 
-                                                            key={match.eventId} 
-                                                            hover 
-                                                            sx={{ cursor: "pointer" }} 
-                                                            onClick={() => handleRowClick(match)}
-                                                            >
-                                                            <TableCell sx={{ width: "300px"}}>{convertDateUTCToLocal(match.eventDate, "index")}</TableCell>
-                                                            <TableCell sx={{ width: "500px"}}>{match.eventName}</TableCell>
-                                                            <TableCell sx={{ width: "200px"}}>
-                                                                <Chip
-                                                                    label={getStatusLabel(match.status)}
-                                                                    sx={{
-                                                                    backgroundColor: getStatusColor(match.status),
-                                                                    color: "white",
-                                                                    fontWeight: "bold",
-                                                                    }}
-                                                                />
-                                                            </TableCell>
-                                                        </TableRow>
-                                                        ))}
-                                                        </TableBody>
-                                                    </Table>
-                                                    </TableContainer> */}
-                                                    {/* <Table>
-                                                        {matches.map((match) =>{
-                                                            <tbody>
-                                                                <tr>
-                                                                    <td>
-                                                                    {`${match.eventName} ${getStatusLabel(match.status)} ${convertDateUTCToLocal(match.eventDate, "index")}`}
-                                                                    </td>
-                                                                </tr>
-                                                            </tbody>
-                                                        })}
-                                                    </Table> */}
-                                                    <Table className='mb-0' responsive hover>
+                                                       <Table className='mb-0' responsive hover>
                                                             <tbody>
                                                                 {matches.map((match) =>{
-                                                                    return(<tr onClick={() => handleRowClick(match)} className="px-1 mx-2 data-provider-table-row" style={{border: '1px solid gray' }}>
+                                                                    return(<tr onClick={() => handleRowClick(match)} className="px-1 data-provider-table-row" style={{border: '1px solid gray' }}>
                                                                     <td className='p-0 ps-1 m-0'>
                                                                         <div className=''>{match.eventName} <span style={{color: getStatusColor(match.status)}}> {getStatusLabel(match.status)} </span> {convertDateUTCToLocal(match.eventDate, "index")}</div>
                                                                     </td>
