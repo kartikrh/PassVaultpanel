b9c6f9fcfa2e46376ab5782605a56140a1a5e6e5
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2025-04-02 16:13:42 +0530

    added country code page

---
+++ src/Pages/CountryCode/index.js
@@ -0,0 +1,295 @@
+import React, { useState, useEffect, useRef } from "react";
+import Breadcrumbs from "../../components/Common/Breadcrumb";
+import Table from "../../components/Common/Table";
+import { Avatar } from "antd";
+import { Container } from "reactstrap";
+import DeleteTabModel from "../../components/Model/DeleteModel";
+import SpinnerModel from "../../components/Model/SpinnerModel";
+import axiosInstance from "../../Features/axios";
+import { useNavigate } from "react-router-dom";
+import { isEqual } from "lodash";
+import {
+  TAB_COUNTRY_CODE,
+  PERMISSION_ADD,
+  PERMISSION_DELETE,
+  PERMISSION_EDIT,
+  PERMISSION_VIEW,
+  SUCCESS,
+  ERROR,
+  MODULE_COUNTRY_CODE,
+} from "../../components/Common/Const";
+import { useDispatch, useSelector } from "react-redux";
+import { checkPermission } from "../../components/Common/Reusables/reusableMethods";
+import { updateToastData } from "../../Features/toasterSlice";
+import LoadDataModal from "../../components/Model/LoadDataModal";
+
+const Index = () => {
+  const pageName = TAB_COUNTRY_CODE;
+  const finalizeRef = useRef(null);
+  const permissionObj = useSelector((state) => state.auth?.tabPermissionList);
+  document.title = TAB_COUNTRY_CODE;
+  const [data, setData] = useState([]);
+
+  const [dataIndexList, setDataIndexList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [deleteModelVisable, setDeleteModelVisable] = useState(false);
+  const [checekedList, setCheckedList] = useState([]);
+  const [loadDataModelVisable, setLoadDataModelVisable] = useState(false);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const fetchData = async (latestValueFromTable) => {
+    setIsLoading(true);
+    const tableActions = finalizeRef.current.getTableAction();
+    await axiosInstance
+      .post(`/admin/countryCode/all`, {
+        ...(latestValueFromTable || tableActions),
+      })
+      .then((response) => {
+        const apiData = response?.result;
+        let apiDataIdList = [];
+        apiData.forEach((ele) => {
+          apiDataIdList.push(ele?.id);
+        });
+        setData(apiData);
+        setDataIndexList(apiDataIdList);
+        setCheckedList([]);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+      });
+  };
+
+  const handleSingleCheck = (e) => {
+    let updateSingleCheck = [];
+    if (checekedList.includes(e.id)) {
+      updateSingleCheck = checekedList.filter((item) => item !== e.id);
+    } else {
+      updateSingleCheck = [...checekedList, e.id];
+    }
+    setCheckedList(updateSingleCheck);
+  };
+
+  const handleLoadData = async (password) => {
+    setIsLoading(true);
+    await axiosInstance
+      .post(`/loadPanelData`, {module: [MODULE_COUNTRY_CODE], password})
+      .then((response) => {
+        fetchData();
+        setLoadDataModelVisable(false);
+        dispatch(
+          updateToastData({
+            data: response?.message,
+            title: response?.title,
+            type: SUCCESS,
+          })
+        );
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        dispatch(
+          updateToastData({
+            data: error?.message,
+            title: error?.title,
+            type: ERROR,
+          })
+        );
+      });
+  };
+
+  const handleDelete = async (e) => {
+    setIsLoading(true);
+    await axiosInstance
+      .post(`/admin/countryCode/delete`, {
+        id: checekedList,
+      })
+      .then((response) => {
+        fetchData();
+        setDeleteModelVisable(false);
+        dispatch(
+          updateToastData({
+            data: response?.message,
+            title: response?.title,
+            type: SUCCESS,
+          })
+        );
+        setCheckedList([]);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        dispatch(
+          updateToastData({
+            data: error?.message,
+            title: error?.title,
+            type: ERROR,
+          })
+        );
+        setCheckedList([]);
+      });
+  };
+  const handleEdit = (id) => {
+    navigate("/addCountryCode", { state: { countryCodeId: id } });
+  };
+  const handleReset = (value) => {
+    fetchData(value);
+  };
+  const columns = [
+    {
+      title: (
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            name="chk_child"
+            value="option1"
+            checked={
+              data?.length > 0 &&
+              isEqual(checekedList?.sort(), dataIndexList?.sort())
+            }
+            onChange={() => {
+              setCheckedList(
+                isEqual(checekedList?.sort(), dataIndexList?.sort())
+                  ? []
+                  : dataIndexList
+              );
+            }}
+          />
+        </div>
+      ),
+      render: (text, record) => (
+        <div className="form-check d-flex align-items-center justify-between">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            name="chk_child"
+            value="option1"
+            checked={checekedList.includes(record.id)}
+            onChange={() => {
+              handleSingleCheck(record);
+            }}
+          />
+          {/* <i className="bx bx-move ms-1 mt-1"></i> */}
+        </div>
+      ), // Use 'select' as a placeholder key for the checkbox column
+      key: "select",
+      style: { width: "2%" },
+    },
+    checkPermission(permissionObj, pageName, PERMISSION_EDIT) && {
+      title: "Edit",
+      key: "edit",
+      render: (text, record) => (
+        <i
+          className="bx bx-edit"
+          onClick={() => {
+            handleEdit(record.id);
+          }}
+        ></i>
+      ),
+      style: { width: "2%", textAlign: "center" },
+    },
+    {
+      title: "Flag",
+      dataIndex: "flag",
+      printType: "ignore",
+      render: (text, record) => (
+        <div className="flex-shrink-0">
+          {text ? (
+            <div>
+              <img
+                className="avatar-sm rounded-circle"
+                alt=""
+                src={text}
+              />
+            </div>
+          ) : (
+            <Avatar src="#" alt="ET">
+              Image
+            </Avatar>
+          )}
+        </div>
+      ),
+      key: "image",
+      style: { width: "10%", textAlign: "left" },
+    },
+    {
+      title: "Code",
+      dataIndex: "countryCode",
+      key: "countryCode",
+      style: { width: "10%" },
+      sort: true,
+    },
+    {
+      title: "Country",
+      dataIndex: "countryName",
+      key: "countryName",
+      style: { width: "76%" },
+      sort: true,
+    },
+  ];
+  const tableElement = {
+    title: "Country Code",
+    reloadButton: true,
+    loadData: true,
+  };
+
+  useEffect(() => {
+    if (!checkPermission(permissionObj, pageName, PERMISSION_VIEW)) {
+      navigate("/dashboard")
+    }
+    fetchData();
+  }, []);
+
+  const handleReload = (value) => {
+    fetchData();
+  };
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid={true}>
+          <Breadcrumbs title="ScoreCard" breadcrumbItem="Country Code" />
+          {isLoading && <SpinnerModel />}
+          <Table
+            ref={finalizeRef}
+            columns={columns}
+            dataSource={data}
+            tableElement={tableElement}
+            deleteModelFunction={setDeleteModelVisable}
+            singleCheck={checekedList}
+            onAddNavigate={"/addCountryCode"}
+            handleReset={handleReset}
+            handleReload={handleReload}
+            loadDataModelFunction={setLoadDataModelVisable}
+            reFetchData={fetchData}
+            isAddPermission={checkPermission(
+              permissionObj,
+              pageName,
+              PERMISSION_ADD
+            )}
+            isDeletePermission={checkPermission(
+              permissionObj,
+              pageName,
+              PERMISSION_DELETE
+            )}
+          />
+          <DeleteTabModel
+            deleteModelVisable={deleteModelVisable}
+            setDeleteModelVisable={setDeleteModelVisable}
+            handleDelete={handleDelete}
+            singleCheck={checekedList}
+          />
+          {loadDataModelVisable && 
+            <LoadDataModal
+              loadDataModelVisable={loadDataModelVisable}
+              setLoadDataModelVisable={setLoadDataModelVisable}
+              handleLoadData={handleLoadData}
+              moduleName={"Country Code"} 
+            />}
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Index;
