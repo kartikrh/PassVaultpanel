2a67406b80e93ae914e991f5f1dce293913ae0fa
SrinivasUpputuri <srinivas@ziwotech.com>
2025-04-28 13:20:19 +0530

    added the logs button and changed the styles for the player in open market

--- src/Pages/Commentary/OpenMarket.jsx
+++ src/Pages/Commentary/OpenMarket.jsx
@@ -2190,6 +2190,7 @@ export const OpenMarket = () => {
                                         handleSingleAction={handleSingleAction}
                                         updateRecordsFunc={updateRecords}
                                         teamsData={teamsData}
+                                        handleDS={handleDS}
                                     />
                                 )}
                             </CardBody>
