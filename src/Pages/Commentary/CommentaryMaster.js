74a6f3584336af155f72bf76395dd6ff959d3345
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2025-05-28 11:56:42 +0530

    added ball start in commentary toss & player selection

--- src/Pages/Commentary/CommentaryMaster.js
+++ src/Pages/Commentary/CommentaryMaster.js
@@ -487,7 +487,7 @@ function CommentaryMaster() {
                                 {commentaryList === 'commentary' && <Button color="primary" className="mx-1" onClick={handleLoadCommentaryClick}>Load Commentary</Button>}
                                 <Button color="primary" className="mx-1" onClick={openIframePopup}>Scorecard</Button>
                                 <Button color="primary" className="mx-1 my-2 my-md-0" onClick={() => {setIsNewUi(!isNewUi)}}>New Ui</Button>
-                                <Button color="primary" className="mx-1 my-2 my-md-0" onClick={handleBallStartClick}>Ball Start</Button>
+                                {(ALL_SCREENS[currentScreen] === COMMENTARY_TOSS_SCREEN || ALL_SCREENS[currentScreen] === COMMENTARY_PLAYER_SELECTION_SCREEN) ? <Button color="primary" className="mx-1 my-2 my-md-0" onClick={handleBallStartClick}>Ball Start</Button> : null}
                               </div>
                             </div>
                           {/* // } */}
