0a351ac975b1affe9f265815a238c9b5fb49996c
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2025-06-04 11:52:24 +0530

    modified socket commUpdate

--- src/Pages/Commentary/Commentary.js
+++ src/Pages/Commentary/Commentary.js
@@ -1992,10 +1992,10 @@ const Commentary = (props) => {
                     batsmanAverage: player?.batsmanAverage,
                 }));
                 
-                socket.emit(COMMENTARY_UPDATE, { ballStatus: ballStatus, eventRefId: commentaryDetails?.eventRefId, commentaryId: commentaryDetails?.commentaryId, teamId: _teams?.[BATTING_TEAM]?.teamId || teams?.[BATTING_TEAM]?.teamId, wicket: _teams?.[BATTING_TEAM]?.teamWicket || teams?.[BATTING_TEAM]?.teamWicket, score: _teams?.[BATTING_TEAM]?.teamScore || teams?.[BATTING_TEAM]?.teamScore , over: _teams?.[BATTING_TEAM]?.teamOver || teams?.[BATTING_TEAM]?.teamOver, playersList: filteredPlayersList });
+                socket.emit(COMMENTARY_UPDATE, { ballStatus: ballStatus, eventRefId: commentaryDetails?.eventRefId, commentaryId: commentaryDetails?.commentaryId, teamId: _teams?.[BATTING_TEAM]?.teamId || teams?.[BATTING_TEAM]?.teamId, wicket: _teams?.[BATTING_TEAM]?.teamWicket || teams?.[BATTING_TEAM]?.teamWicket, score: _teams?.[BATTING_TEAM]?.teamScore || teams?.[BATTING_TEAM]?.teamScore , over: _teams?.[BATTING_TEAM]?.teamOver || teams?.[BATTING_TEAM]?.teamOver, oversPerInings: matchTypeDetails?.oversPerInings, playersList: filteredPlayersList });
             }
         }
-    }, [commentaryDetails, ballStatus, currentOver?.ballCount]);
+    }, [commentaryDetails, ballStatus, matchTypeDetails?.oversPerInings, currentOver?.ballCount]);
 
     useEffect(() => {
         if (isUndoCompleted) {
