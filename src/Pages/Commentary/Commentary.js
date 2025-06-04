b413ae3dc67e823d4b252b71b2ca5199770fa069
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2025-06-04 11:34:27 +0530

    modified socket commUpdate

--- src/Pages/Commentary/Commentary.js
+++ src/Pages/Commentary/Commentary.js
@@ -1995,7 +1995,7 @@ const Commentary = (props) => {
                 socket.emit(COMMENTARY_UPDATE, { ballStatus: ballStatus, eventRefId: commentaryDetails?.eventRefId, commentaryId: commentaryDetails?.commentaryId, teamId: _teams?.[BATTING_TEAM]?.teamId || teams?.[BATTING_TEAM]?.teamId, wicket: _teams?.[BATTING_TEAM]?.teamWicket || teams?.[BATTING_TEAM]?.teamWicket, score: _teams?.[BATTING_TEAM]?.teamScore || teams?.[BATTING_TEAM]?.teamScore , over: _teams?.[BATTING_TEAM]?.teamOver || teams?.[BATTING_TEAM]?.teamOver, playersList: filteredPlayersList });
             }
         }
-    }, [commentaryDetails, ballStatus]);
+    }, [commentaryDetails, ballStatus, currentOver?.ballCount]);
 
     useEffect(() => {
         if (isUndoCompleted) {
