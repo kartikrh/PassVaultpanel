cfd0b74261586e68cd6bba4d1d6a16fba57d4e84
SrinivasUpputuri <srinivas@ziwotech.com>
2025-09-27 16:30:05 +0530

    super over issue changes

--- src/Pages/Commentary/Commentary.jsx
+++ src/Pages/Commentary/Commentary.jsx
@@ -17,6 +17,12 @@ export const CommentaryScreen = ({
     players, currentOver, currentInnings, isPredict, isPredictToggle, setIsPredictToggle, allteams, fetchData, isSaving, isAnyPopupOpen, overTypeOption, overTypeValue, onOverTypeChange, handleDefaultOverSwitch, isDefaultOverType }) => {
     const [actionPopup, setActionPopup] = useState(undefined);
     const isDarkTheme = document.body.getAttribute('data-theme') === 'dark';
+    
+    useEffect(() => {
+        if(currentInnings == 0){
+            handleRemainingBallsShowToggle()
+        }
+    }, [currentInnings])
 
     const OffSymbolStatus = () => {
         return (
