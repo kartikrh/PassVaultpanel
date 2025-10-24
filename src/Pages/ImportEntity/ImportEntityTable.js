19503aeae26d3776b5269d1324703f3f9b363a3e
SrinivasUpputuri <srinivas@ziwotech.com>
2025-10-24 17:34:28 +0530

    Added the page size as max 50

--- src/Pages/ImportEntity/ImportEntityTable.js
+++ src/Pages/ImportEntity/ImportEntityTable.js
@@ -720,7 +720,7 @@ export default function ImportEntity() {
     reloadButton: true,
   };
   // console.log("Length: ", data?.length||0)
-  const customPageSizeOptions = ["10", "20", "50", "100"];
+  const customPageSizeOptions = ["10", "20", "50"];
   return (
     <React.Fragment>
       <div className="page-content">
