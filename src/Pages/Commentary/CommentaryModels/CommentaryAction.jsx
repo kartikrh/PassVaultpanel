f118f87e2d739a4974d4fb38818b3380f95acda9
ishaWpa <isha.sidhu.wpa@gmail.com>
2025-11-13 10:44:10 +0530

    button changes

--- src/Pages/Commentary/CommentaryModels/CommentaryAction.jsx
+++ src/Pages/Commentary/CommentaryModels/CommentaryAction.jsx
@@ -95,11 +95,11 @@ const CommentaryAction = ({
                             <img className="button-icon" src="icons/r.png" alt="Icon" />
                             etired Hurt
                         </Col>
-                        {/* {showInningsButton && <Col role="button" className="score-button yellow-information-button" xs={4} md={4} lg={4}
+                        {showInningsButton && <Col role="button" className="score-button yellow-information-button" xs={4} md={4} lg={4}
                             onClick={() => toggleUndoInnings()}>
                             <img className="button-icon" src="icons/revert.png" alt="Icon" />
                             Undo Innings
-                        </Col>} */}
+                        </Col>}
                     </Row>
                 </ModalBody>
                 <ModalFooter>
