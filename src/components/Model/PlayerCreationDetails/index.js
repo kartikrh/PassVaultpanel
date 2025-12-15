8389979da96dc595bcc9924de923761a8078f4de
ishaWpa <isha.sidhu.wpa@gmail.com>
2025-12-15 17:35:02 +0530

    creationDetails popup in players

---
+++ src/components/Model/PlayerCreationDetails/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
+
+export const PlayerCreationDetails = ({ isOpen, toggle, createdDetailsData }) => {
+
+    return (
+        <Modal isOpen={isOpen} toggle={toggle} size="md" centered>
+            <ModalHeader toggle={toggle}>Player Creation Details
+            </ModalHeader>
+            <ModalBody>
+                <div className="p-2">
+                    <div className="mb-1">
+                        <strong>Player Name:</strong>
+                        <span className="ms-2">{createdDetailsData?.playerName || "N/A"}</span>
+                    </div>
+                    <div className="mb-1">
+                        <strong>Created Date:</strong>
+                        <span className="ms-2">
+                            {createdDetailsData?.createdDate
+                                ? new Date(createdDetailsData.createdDate).toLocaleString()
+                                : 'N/A'}
+                        </span>
+                    </div>
+                    <div className="mb-1">
+                        <strong>Created By:</strong>
+                        <span className="ms-2">{createdDetailsData?.createdBy || 'N/A'}</span>
+                    </div>
+                </div>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="primary" onClick={toggle}>
+                    OK
+                </Button>
+            </ModalFooter>
+        </Modal>
+    );
+};
