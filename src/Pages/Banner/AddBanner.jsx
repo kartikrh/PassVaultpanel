8d8ad04eae69336c0e211b0adfb32491c23e42b6
ishaWpa <isha.sidhu.wpa@gmail.com>
2026-06-13 19:15:39 +0530

    added logs in banner

--- src/Pages/Banner/AddBanner.jsx
+++ src/Pages/Banner/AddBanner.jsx
@@ -74,11 +74,18 @@ const AddBanner = () => {
     }
   }, [isSaved]);
 
+  useEffect(() => {
+    console.log("[Banner] initialEditData", initialEditData);
+  }, [initialEditData]);
+
   const fetchData = async (bannerId) => {
+    console.log("[Banner] Fetching bannerId:", bannerId);
+
     await axiosInstance
       .post("/admin/banner/byId", { bannerId })
       .then((response) => {
         // setInitialEditData(response?.result);
+        console.log("[Banner] API response", response?.result);
         const data = response?.result;
 
         setInitialEditData({
@@ -139,6 +146,14 @@ const AddBanner = () => {
   const handleBackClick = () => {
     navigate("/banner");
   };
+
+  useEffect(() => {
+    console.log("[Banner] FormBuilder Props", {
+      initialEditData,
+      masterData,
+      fieldsCount: fields?.length,
+    });
+  }, [initialEditData, masterData, fields]);
 
   return (
     <React.Fragment>
